2c003b7ae6b2344c360bf7212486d8d6562562f6
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-08-31 18:37:38 +0530

    Typescript started

---
+++ main.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var message = "Hello World Welocme";
+console.log(message);
+var x = 20;
+// let x=30;
+var y = 40;
+var sum;
+//  const title
+var isBeginner = true;
+var total = 0;
+var name = "Mayuri Gurav";
+// name= true
+var sentence = "My Name is ".concat(name, ", I am begginer in Typescript");
+console.log(sentence);
+var n = null;
+var u = undefined;
+var isNew = null;
+var myName = undefined;
+var list1 = [1, 2, 3];
+var list2 = [4, 5, 6];
---
+++ main.ts
@@ -0,0 +1,27 @@
+export {};
+
+let message = "Hello World Welocme";
+console.log(message);
+
+let x = 20;
+// let x=30;
+
+const y = 40;
+let sum;
+//  const title
+
+let isBeginner: boolean = true;
+let total: number = 0;
+
+let name: string = "Mayuri Gurav";
+// name= true
+let sentence: string = `My Name is ${name}, I am begginer in Typescript`;
+console.log(sentence);
+
+let n: null = null;
+let u: undefined = undefined;
+let isNew: boolean = null;
+let myName: string = undefined;
+
+let list1: number[] = [1, 2, 3];
+let list2: Array<number> = [4, 5, 6];
