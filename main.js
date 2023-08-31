5bf63bc3bc0931d4d1719d1a7321c2144b042c8a
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-08-31 19:50:31 +0530

    variable types in typescript

--- main.js
+++ main.js
@@ -15,7 +15,27 @@ var sentence = "My Name is ".concat(name, ", I am begginer in Typescript");
 console.log(sentence);
 var n = null;
 var u = undefined;
-var isNew = null;
-var myName = undefined;
+// let isNew: boolean = null;
+// let myName: string = undefined;
 var list1 = [1, 2, 3];
 var list2 = [4, 5, 6];
+//tuple
+var person1 = ["Chris", 22];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 6] = "Red";
+    Color[Color["Green"] = 7] = "Green";
+    Color[Color["Blue"] = 8] = "Blue";
+})(Color || (Color = {}));
+var c = Color.Green;
+console.log(c); //7
+var randomValue = 21;
+randomValue = true;
+randomValue = "Meghana";
+var myvariable = 10;
+// console.log(myvariable.name);
+// myvariable();
+// myvariable.toLowerCase();
+var myvariable1 = 10;
+// console.log(myvariable1.name);
+myvariable1.toLowerCase();
--- main.ts
+++ main.ts
@@ -20,8 +20,32 @@ console.log(sentence);
 
 let n: null = null;
 let u: undefined = undefined;
-let isNew: boolean = null;
-let myName: string = undefined;
+// let isNew: boolean = null;
+// let myName: string = undefined;
 
 let list1: number[] = [1, 2, 3];
 let list2: Array<number> = [4, 5, 6];
+
+//tuple
+let person1: [string, number] = ["Chris", 22];
+
+enum Color {
+  Red = 6,
+  Green,
+  Blue,
+}
+let c: Color = Color.Green;
+console.log(c); //7
+
+let randomValue: any = 21;
+randomValue = true;
+randomValue = "Meghana";
+
+let myvariable: any = 10;
+// console.log(myvariable.name);
+// myvariable();
+// myvariable.toLowerCase();
+
+let myvariable1: unknown = 10;
+// console.log(myvariable1.name);
+(myvariable1 as string).toLowerCase();
